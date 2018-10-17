f9b0aa896a79edf954a86d54d3516c18e8d5c567
zhangkaixin <zhangkaixin@jiayunhun.com>
2018-10-17 11:18:57 +0800

    更webpack配置

--- demo-ssr/src/main.js
+++ demo-ssr/src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter } from "./router";
 import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+export function createApp () {
+  const router = createRouter();
+  const app = new Vue({
+    router,
+    store,
+    // 根实例简单的渲染应用程序组件。
+    render: h => h(App)
+  })
+  return { app }
+}
