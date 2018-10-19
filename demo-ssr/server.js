061143127750e0ab3c2e073d221921aba28304a0
zhangkaixin <zhangkaixin@jiayunhun.com>
2018-10-19 14:49:04 +0800

    ssr 基本框架

--- demo-ssr/server.js
+++ demo-ssr/server.js
@@ -1,33 +1,72 @@
-// server.js
-// 第 1 步：创建一个 Vue 实例
-const Vue = require("vue");
+const fs = require("fs");
+// const Koa = require("koa");
+const path = require("path");
+// const koaStatic = require('koa-static')
+// const app = new Koa();
+const express = require('express');
 
-const Koa = require("koa");
-const app = new Koa();
-// 第 2 步：创建一个 renderer
-const renderer = require("vue-server-renderer").createRenderer();
+const server = express();
 
+const resolve = file => path.resolve(__dirname, file);
+// 开放dist目录
+// app.use(koaStatic(resolve('./dist')))
 
-// 第 3 步：添加一个中间件来处理所有请求
-app.use(async (ctx, next) => {
-  const vm = new Vue({
-    data: {
-      title: "ssr example",
-      url: ctx.url
-    },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
-  });
-  // 将 Vue 实例渲染为 HTML
-  renderer.renderToString(vm, (err, html) => {
-    if(err){
-      res.status(500).end('Internal Server Error')
-      return
-    }
-    ctx.body = html
-  });
+// 第 2 步：获得一个createBundleRenderer
+const { createBundleRenderer } = require("vue-server-renderer");
+const bundle = require("./dist/vue-ssr-server-bundle.json");
+const clientManifest = require("./dist/vue-ssr-client-manifest.json");
+
+const renderer = createBundleRenderer(bundle, {
+  runInNewContext: false,
+  template: fs.readFileSync(resolve("./src/index.temp.html"), "utf-8"),
+  clientManifest: clientManifest
 });
+
+
+server.get('/', (req, res) => {
+  const context = { 
+    title: "ssr test",
+    url: req.url 
+  }
+  // 这里无需传入一个应用程序，因为在执行 bundle 时已经自动创建过。
+  // 现在我们的服务器与应用程序已经解耦！
+  renderer.renderToString(context, (err, html) => {
+    console.log(err, html)
+    // 处理异常……
+    res.end(html)
+  })
+})
 
 const port = 3000;
-app.listen(port, function() {
+server.listen(port, function() {
   console.log(`server started at localhost:${port}`);
 });
+
+
+// function renderToString(context) {
+//   return new Promise((resolve, reject) => {
+//     renderer.renderToString(context, (err, html) => {
+//       err ? reject(err) : resolve(html);
+//     });
+//   });
+// }
+// // 第 3 步：添加一个中间件来处理所有请求
+// app.use(async (ctx, next) => {
+//   const context = {
+//     title: "ssr test",
+//     url: ctx.url
+//   };
+//   // 将 context 数据渲染为 HTML
+//   const html = await renderToString(context);
+//   ctx.body = html;
+// });
+
+// const port = 3000;
+// app.listen(port, function() {
+//   console.log(`server started at localhost:${port}`);
+// });
+// const serve = (path, cache) => express.static(resolve(path), { maxAge: cache ? 0 : 0 });
+
+server.use(express.static(path.join(__dirname, 'dist')));
+// server.use('/js', serve('./dist/js', true));
+// server.use('/css', serve('./dist/css', true));
--- demo-ssr/src/main.js
+++ demo-ssr/src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createRouter } from "./router";
-import store from './store'
+import { createStore } from "./store";
 
 Vue.config.productionTip = false
 
 export function createApp () {
   const router = createRouter();
+  const store = createStore()
   const app = new Vue({
     router,
     store,
     // 根实例简单的渲染应用程序组件。
     render: h => h(App)
   })
-  return { app }
+  return { app, router, store }
 }
